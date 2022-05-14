6f6750a81ad70605ca6f8b30fb92456dbb519012
Matt <matthew.t.kissinger@gmail.com>
2022-05-14 16:34:51 -0700

    initial setup of webpack with project

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//create a class system and styling system
+
+console.log('hello');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZHJvcC1kb3duLW1lbnUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy9jcmVhdGUgYSBjbGFzcyBzeXN0ZW0gYW5kIHN0eWxpbmcgc3lzdGVtXG5cbmNvbnNvbGUubG9nKCdoZWxsbycpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
