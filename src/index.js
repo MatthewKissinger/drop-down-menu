63e47da85b2a3f3daf6e1dd23160abb06e7b764c
Matt <matthew.t.kissinger@gmail.com>
2022-05-18 22:21:24 -0700

    refactored the module so that the drop menu is triggered via javascript instead of css hover

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 //create a class system and styling system
 import './main.scss';
+
+// DOM Cache
+let dropContainer = document.querySelector('.drop-container');
+
+let dropMenu = document.querySelector('.drop-menu');
+
+dropContainer.addEventListener('mouseover', function(e) {
+    if (!e.target.matches('li')) {
+        dropMenu.classList.remove('hide');
+    }
+})
+
+dropContainer.addEventListener('mouseleave', function() {
+    dropMenu.classList.add('hide');
+})
+
